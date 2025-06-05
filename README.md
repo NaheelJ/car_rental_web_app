e7211597c48d7f97ba07d81723201e6a5e121b7f
Naheelj <naheeljnaheel5454@gmail.com>
2025-06-05 20:35:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# car_rental_web_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/car_rental_web_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.car_rental_web_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
